f93020319e3e1e886719dc16d60c714e438e811d
דור שרעבי <dor29494@gmail.com>
2021-02-17 15:30:21 +0200

    Switch routers + Global State manager + Icons + Export Functions file

--- src/stateManager/stateManager.js
+++ src/stateManager/stateManager.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 
 const TriviaContext = React.createContext();
 const { Provider } = TriviaContext;

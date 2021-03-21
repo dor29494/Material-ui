6ed58b98f8cc56018696f24532b8b5ee8b572cdc
דור שרעבי <dor29494@gmail.com>
2021-03-21 12:03:34 +0200

    21-03-2021 push

--- src/App.js
+++ src/App.js
@@ -1,79 +1,132 @@
-import React,{useState,useContext} from "react"
-import {TriviaContext} from "./stateManager/stateManager"
-import Home from "./Home/Home"
-import Online from "./Online/Online"
-import {useStyles,listProvider,theme} from "./services/exportsFile"
-import { BrowserRouter as Router, Switch, Route ,useHistory, Link} from "react-router-dom";
-import {ThemeProvider} from '@material-ui/core/styles'
-import {Container,AppBar,Toolbar,Typography,Drawer} from '@material-ui/core'
-import Iconbutton from "@material-ui/core/Iconbutton"
-import MenuIcon from "@material-ui/icons/Menu"
-import './App.css';
-
-
-
+import React, { useContext, useEffect } from "react";
+import Home from "./Home/Home";
+import TopBar from "./TopBar/TopBar";
+import Login from "./Login/Login";
+import { useStyles} from "./services/exportsFile";
+import { playersLogin } from "./stateManager/atoms";
+import styled from "styled-components";
+import io from "socket.io-client";
+import "./App.css";
+import { Container} from "@material-ui/core";
+import { useRecoilState} from "recoil";
+import { Route,useHistory} from "react-router-dom";
+import Show from "./Show/Show";
+import firebase from "firebase/app";
+import "firebase/auth";
+import {
+  accountState,
+  displayState,
+  appLoading,
+  socketAlertState,
+  alertState,
+} from "./stateManager/atoms";
 
 function App() {
-  let classes = useStyles()
-  const {
-    menuSides,
-    boleanDirection,
-    setBoleanDirection,
-  } = useContext(TriviaContext);
-  
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-    setBoleanDirection({ ...boleanDirection, [anchor]: open });
-  };
+  const socket = io("https://reveal-multiplex.glitch.me/");
+  const [display, setDisplay] = useRecoilState(displayState);
+  const [account, setAccount] = useRecoilState(accountState);
+  const [login, setLogin] = useRecoilState(playersLogin);
+  const [loading, setLoading] = useRecoilState(appLoading);
+  const [socketAlerts, setSocketAlerts] = useRecoilState(socketAlertState);
+  const [alertObj, setAlert] = useRecoilState(alertState);
+
+
+  let classes = useStyles();
+  let history = useHistory();
+
+  useEffect(() => {
+    let localStorageObject = JSON.parse(localStorage.getItem("SnookerPass"));
+    console.log(">>>>", localStorageObject);
+    if (localStorageObject) {
+      const { password, email, username } = localStorageObject;
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          var user = userCredential.user;
+          setLogin(true);
+          setLoading(false);
+          setAccount(localStorageObject);
+          setAlert({
+            alert: false,
+            alertMsg: '',
+          })
+          setSocketAlerts({
+            alert: false,
+            alertMsg: '',
+          })
+          history.push("/");
+          setLoading(false)
+        })
+        .catch((error) => {
+          localStorage.removeItem("SnookerPass");
+          console.log(error)
+          var signInErrorCode = error.code;
+          var signInerrorMessage = error.message;
+          history.push("/signin");
+        });
+    } 
+  }, []);
+  useEffect(() => {
+    socket.on("3fbc77dac0c245be", function (data) {
+      // console.log(data);
+      // ignore data from sockets that aren't ours
+      if (data.socketId !== "3fbc77dac0c245be") {
+        return console.log("WRONG");
+      }
+      // console.log('step 3 , listeing alert...', data.state.alert)
+      //const {incomingData} = data;
+      // incomingData.state.score = score
+      // console.log(data.state.alert);
+      setSocketAlerts(data.state.alert);
+      setDisplay(data.state.score);
+    });
+
+    socket.on("connect", (data) => {
+      if (data) {
+        console.log("On is ON!");
+        console.log(data);
+      }
+    });
+    console.log("finish");
+  }, []);
 
   return (
-    <div className="App">
-       <ThemeProvider 
-    theme={theme}
-    className={classes.root}>
-         <Container maxWidth="sm">
-       <AppBar position="sticky">
-         <Toolbar>
-           <Iconbutton
-           onClick={toggleDrawer(menuSides[1], true)}
-           aria-label="open drawer"
-           edge="start">
-             <MenuIcon
-             />
-           </Iconbutton>
-           <Typography variant="h6">
-             Trivo
-             </Typography>
-         </Toolbar>
-       </AppBar>
-       <div>
-      {menuSides.map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Drawer anchor={anchor} open={boleanDirection[anchor]} onClose={toggleDrawer(anchor, false)}>
-            {listProvider(anchor,toggleDrawer,classes)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+    <div className={classes.root}>
+      <TopBar classes={classes} />
+      <Route path="/show">
+        <Show classes={classes} />
+      </Route>
+      <Route path="/signin">
+        <Login classes={classes} />
+      </Route>
+      <Route exact path="/">
+        <Container fixed justify="center" className={classes.appWrapper}>
+          <Home classes={classes} socket={socket} />
+        </Container>
+      </Route>
     </div>
- 
-</Container>
-</ThemeProvider>
-
-
-
-{/* Switch Routers! */}
-        <Switch>
-          <Route path="/online">
-            <Online />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10vw;
+  background: lightgrey;
+  height: 30vw;
+  width: 30vw;
+`;
+const BodyWrapper = styled.div`
+  font-size: "RocknRoll One", sans-serif;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+  left: 0;
+  background: red;
+  height: 100vw;
+  width: 100vw;
+`;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,53 @@ import {BrowserRouter as Router} from "react-router-dom"
 import {StateManager} from "./stateManager/stateManager";
 import './index.css';
 import App from './App';
+import {theme} from "./services/exportsFile"
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider} from '@material-ui/core/styles/'
+import firebase from "firebase/app";
+import {
+  RecoilRoot,
+} from 'recoil';
+import "firebase/storage";
+
+function initialize() {
+  // [START storage_initialize]
+  // Set the configuration for your app
+  // TODO: Replace with your app's config object
+  var firebaseConfig = {
+    apiKey: "AIzaSyBwQchV2xy7faO8GAbwsUVt7xlTHYQxPtM",
+    authDomain: "snooker-scoreboard-a3bc3.firebaseapp.com",
+    projectId: "snooker-scoreboard-a3bc3",
+    storageBucket: "snooker-scoreboard-a3bc3.appspot.com",
+    messagingSenderId: "351656188668",
+    appId: "1:351656188668:web:a5533c70b3ebac3f8b1579",
+    measurementId: "G-0E6QMGZ7VG"
+  };
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+ }else {
+    firebase.app(); // if already initialized, use that one
+ }
+
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  var storage = firebase.storage();
+  // [END storage_initialize]
+}
+initialize()
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <Router>
-      <StateManager>
+      <RecoilRoot>
+        <ThemeProvider
+        theme={theme}>
     <App />
-      </StateManager>
+        </ThemeProvider>
+        </RecoilRoot>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
